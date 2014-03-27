751c969d5c88d58bf4ce0a6a28b4ed8f454a30dd
paul@mccallick.com <paul@mccallick.com>
2014-03-27 09:01:59 -0700

    Added watch capabilities and fixed browserify so it works in the browser again.

--- gruntfile.js
+++ gruntfile.js
@@ -8,16 +8,26 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            scripts: {
+                files: './BiasTab.Web/Scripts/Bias/bias_view_model.js',
+                tasks: ['browserify'],
+            },
+        },
         browserify: {
             js: {
-                src: ['./BiasTab.Web/Scripts/Bias/bias_tab.js'],
-                dest: './BiasTab.Web/Scripts/bundle.js'
+                src: ['./BiasTab.Web/Scripts/Bias/bias_view_model.js'],
+                dest: './BiasTab.Web/Scripts/bundle.js',
+                options: {
+                    alias: ['./BiasTab.Web/Scripts/Bias/bias_view_model.js:bias_view_model']
+                }
             }
-        }
+        },
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
     grunt.registerTask('default', ['browserify']);
